144575bd1eac16e0b76f1a26b15d0c630681803a
Ion <macaria.ion@gmail.com>
2017-05-16 18:18:07 +0300

    last

--- modules/less/src/Controller/LessWatchController.php
+++ modules/less/src/Controller/LessWatchController.php
@@ -47,19 +47,19 @@ class LessWatchController extends ControllerBase {
           
           $theme = $cached_data['less']['theme'];
           
-          $styles = array(
-            '#items' => array(
-              $input_file => $cached_data,
-            ),
-          );
+          $html['page']['#attached']['library'][] = $theme . '/global-styling';
           
-          //$styles = _less_pre_render($styles);
+          //$html = _less_pre_render($html);
           
-          if (filemtime($styles['#items'][$input_file]['data']) > $current_mtime) {
-            $changed_files[] = array(
-              'old_file' => $file_url_parts['path'],
-              'new_file' => file_create_url($styles['#items'][$input_file]['data']),
-            );
+          foreach ($html['page']['#attached']['html_head'] as $element) {
+            if (substr($element[1], 0, 4) == 'less') {
+              if (filemtime($element[0]['#attributes']['uri']) > $current_mtime) {
+                $changed_files[] = array(
+                  'old_file' => $file_url_parts['path'],
+                  'new_file' => file_create_url($element[0]['#attributes']['uri']),
+                );
+              }
+            }
           }
         }
       }
@@ -67,54 +67,4 @@ class LessWatchController extends ControllerBase {
 
     return new JsonResponse($changed_files);
   }
-
-  /* public function _less_watch() {
-    
-    global $theme;
-    
-    
-    
-    
-    if ($config->get(LESS_WATCH) ?: FALSE) {
-      
-      $files = (isset($_POST['less_files']) && is_array($_POST['less_files'])) ? $_POST['less_files'] : array();
-      
-      foreach ($files as $file) {
-        
-        $file_url_parts = drupal_parse_url($file);
-        
-        if ($cache = \Drupal::cache()->get('less:watch:' . \Drupal\Component\Utility\Crypt::hashBase64($file['data']))) {
-          
-          kint($cache);
-          $cached_data = $cache->data;
-          
-          $input_file = $cached_data['less']['input_file'];
-          
-          $output_file = $cached_data['less']['output_file'];
-          
-          $current_mtime = filemtime($output_file);
-          
-          $theme = $cached_data['less']['theme'];
-          
-          $styles = array(
-            '#items' => array(
-              $input_file => $cached_data,
-            ),
-          );
-          
-          $styles = _less_pre_render($styles);
-          
-          if (filemtime($styles['#items'][$input_file]['data']) > $current_mtime) {
-            $changed_files[] = array(
-              'old_file' => $file_url_parts['path'],
-              'new_file' => file_create_url($styles['#items'][$input_file]['data']),
-            );
-          }
-        }
-      }
-    }
-    
-    return $changed_files;
-  } */
-  
 }
